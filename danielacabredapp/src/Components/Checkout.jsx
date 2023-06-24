b9d78c58d6554a653ac9a77fc4583633dc86243b
Daniela Cabred <danielacabred@gmail.com>
2023-06-24 12:35:56 -0300

    checkout

--- danielacabredapp/src/Components/Checkout.jsx
+++ danielacabredapp/src/Components/Checkout.jsx
@@ -4,47 +4,57 @@ import { CartContext } from "../Context/CartContext"
 
 
 const Checkout = () => {
-const [nombre, setNombre]= useState("")
-const [email, setEmail]= useState("")
-const [telefono, setTelefono]= useState("")
-const {carrito, totalCompra}= useContext(CartContext)
+const [nombre, setNombre] = useState("")
+const [email, setEmail] = useState("")
+const [telefono, setTelefono] = useState("")
+const [orden, setOrden]= useState("")
+const {carrito, totalCompra} = useContext(CartContext)
+
+const generarOrden=()=>{
+    const cliente = {nombre: nombre, email: email,telefono: telefono}
+    const productos = carrito.map(item=>({id:item.id, nombre:item.nombre, precio: item.precio}))
+    const fecha = new Date();
+    const date = `${fecha.getDate()}-${fecha.getMonth()+1}-${fecha.getFullYear()} ${fecha.getHours()}:${fecha.getMinutes()}`
+    const total = totalCompra()
+    const orden = {cliente:cliente, productos:productos, date:date, total:total}
+    console.log(orden);
+}
     
 return(
-    <div classNameName="container my-5 ">
-        <div classNameName="row" >
-            <div classNameName="col-md-6">
+    <div className="container my-5 ">
+        <div className="row" >
+            <div className="col-md-6">
                 <form>
                     <div className="mb-3">
-                        <label for="Nombre y Apellido" className="form-label">Nombre y Apellido</label>
-                        <input type="text" className="form-control"/>
+                        <label for="Nombre y Apellido" className="form-label"><b>Nombre y Apellido</b></label>
+                        <input type="text" className="form-control" onInput={(e)=>{setNombre(e.target.value)}}/>
                         
                     </div>
                     <div className="mb-3">
-                        <label for="Email" className="form-label">Email</label>
-                        <input type="text" className="form-control"/>
+                        <label for="Email" className="form-label"><b>Email</b></label>
+                        <input type="text" className="form-control"onInput={(e)=>{setEmail(e.target.value)}}/>
                     </div>
                     <div className="mb-3">
-                        <label for="Teléfono" className="form-label">Teléfono</label>
-                        <input type="number" className="form-control"/>
+                        <label for="Teléfono" className="form-label"><b>Teléfono</b></label>
+                        <input type="number" className="form-control"onInput={(e)=>{setTelefono(e.target.value)}}/>
                     </div>
-                    <button type="submit" classNameName="btn btn-warning">Generar Orden</button>
+                    <button type="submit" className="btn btn-warning" onClick={generarOrden}><b>Generar Orden</b></button>
                 </form>
             </div>
-            <div classNameName="col-md-6">
+            <div className="col-md-6">
                 <tbody>
                     {carrito?.map(item => (
                         <tr key={item.id}>
                             <td><img src={item.imagen} alt={item.nombre} width={80} /></td>
                             <td className="align-middle">{item.nombre}</td>
-                            <td className="align-middle text-end">{item.cantidad} x ${item.precio}</td>
-                            <td className="align-middle text-end">${item.precio*item.cantidad}</td>
+                            <td className="align-middle">{item.cantidad} x ${item.precio}</td>
+                            <td className="align-middle text-center">${item.precio*item.cantidad}</td>
                         </tr>
                     ))
                     }
                     <tr>
-                        <td colSpan={3}>&nbsp;</td>
-                        <td className="text-end"><b>Total a Pagar</b></td>
-                        <td className="text-end"><b> $:{totalCompra()}</b></td>
+                        <td colSpan={2} className="align-middle text-end p-5"><b>Total a Pagar :</b></td>
+                        <td className="align-middle text-center"><b> ${totalCompra()}</b></td>
                     </tr>
                 </tbody>
             </div>
